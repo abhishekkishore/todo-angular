0c8be859789218e7ce9722d5019f42ed8d8870a5
abhishekkishore <5cfe140fd6!>
2019-04-29 15:33:25 +1000

    Create main and side panels.

--- todo-ui/src/app/app.module.ts
+++ todo-ui/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainpanelComponent } from './mainpanel/mainpanel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainpanelComponent
   ],
   imports: [
     BrowserModule
