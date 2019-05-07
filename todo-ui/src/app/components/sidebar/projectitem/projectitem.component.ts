45e7ec8819207fab6722019124bc03e5c357a58b
Abhishek Kishore <abhishek.kishore@noexternalmail.hsbc.com.au>
2019-05-07 16:37:34 +1000

    Implemented initial selection.
    Change-Id: Ib22d0b3c5047658f8e266f9610d7b97002062ec9
    Signed-off-by: Abhishek Kishore <abhishek.kishore@noexternalmail.hsbc.com.au>

--- todo-ui/src/app/components/sidebar/projectitem/projectitem.component.ts
+++ todo-ui/src/app/components/sidebar/projectitem/projectitem.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MessageExchangeComponentService } from "../../../services/message-exchange/message-exchange-component.service";
 
 @Component({
   selector: 'app-projectitem',
   templateUrl: './projectitem.component.html',
   styleUrls: ['./projectitem.component.css']
 })
-export class ProjectitemComponent {
+export class ProjectitemComponent implements OnInit{
 
   private active = false;
   private cursor = "auto";
+  private selected = false;
   
   @Input() tasksLength: string;
   @Input() name: string;
+  @Input() order: string;
 
-  constructor() { }
+  constructor(private messageExchangeComponent: MessageExchangeComponentService) { }
+  
+  ngOnInit() {
+      this.messageExchangeComponent.selectedProjectOrder.subscribe(order=>this.clearSelection(order));
+  }
+  
+  private clearSelection(order: string): void {
+      if(order!=null && order !== this.order) {
+          this.selected = false;
+      }
+      else{
+          this.selected = true;
+      }
+  }
   
   private mouseenter(event: MouseEvent) {
       this.active = true;
@@ -26,6 +42,17 @@ export class ProjectitemComponent {
   }
 
   private onclick(event: MouseEvent) {
-	alert('click');
+	this.active = false;
+//	this.selected = true;
+	this.messageExchangeComponent.projectSelected(this.order);
+  }
+  
+  private firstProject(): boolean {
+      if(this.order==='1') {
+          this.selected = true;
+          this.active = false;
+          return true;
+      }
+      return false;
   }
 }
--- todo-ui/src/app/services/message-exchange/message-exchange-component.service.ts
+++ todo-ui/src/app/services/message-exchange/message-exchange-component.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageExchangeComponentService {
+    
+  private selectedProjectOrderSubject = new BehaviorSubject<string>("1");
+  selectedProjectOrder = this.selectedProjectOrderSubject.asObservable();
 
   constructor() { }
+  
+  public projectSelected(order: string): void {
+      this.selectedProjectOrderSubject.next(order);
+  }
 }
